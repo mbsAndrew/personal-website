2d07e37cc3df5d10b770dc22fed99db59910fc24
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-28 13:59:20 -0400

    Intro

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styles from './intro.module.scss';
+import me from '../../assets/me.jpg';
 
 const Intro = () => {
     return (
-        <>
-            I'm an intro!
-        </>
+        <div className={styles.intro}>
+            <img className={styles.img} src={me} alt={"Andrew Hansen headshot"} />
+            <div className={styles.info}>
+                <p className={styles.subtitle}>
+                    About Me
+                </p>
+                <h2 className={styles.title}>
+                    Front End Designer and Developer
+                </h2>
+                <p className={styles.desc}>
+                    I'm born and raised in New Jersey. I first started my career doing small design work 
+                    to share on social media. Then, I started to learn front end development because I always
+                    had an interest in breaking into the development space.                    
+                </p>
+                <p className={styles.desc}>
+                    Over the past few years, I've been working on improving my skills to create applications using the MERN
+                    stack as well as Next.js and Gatsby. I'm always excited to learn something new and see what I can do with it!                    
+                </p>
+            </div>            
+        </div>
     )
 };
 

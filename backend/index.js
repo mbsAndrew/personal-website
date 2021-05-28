7ad24d65f4e0efe9b21ddcd06593c8477b64c913
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-28 15:38:22 -0400

    more express env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 
 require("dotenv").config();
 var express = require("express");
-
+var cors = require("cors");
 var app = express();
+const { PORT } = process.env;
+app.use(cors());
 app.use("/api", require("./routes/index"));
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});

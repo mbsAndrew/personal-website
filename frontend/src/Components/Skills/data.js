a4605c78e7c91d4c07de7d625ad5140cdcb7d9ff
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-28 13:59:47 -0400

    data for images.

---
+++ frontend/src/Components/Skills/data.js
@@ -0,0 +1,43 @@
+import svgs from '../../assets/logo/index';
+
+
+const data = [
+    {
+        name: "JavaScript",
+        img: svgs.javascript
+    }, 
+    {
+        name: "React",
+        img: svgs.react
+    },    
+    {
+        name: "CSS",
+        img: svgs.css
+    },
+    {
+        name: "Sass",
+        img: svgs.sass
+    },
+    {
+        name: "jQuery",
+        img: svgs.jquery
+    },
+    {
+        name: "MongoDB",
+        img: svgs.mongodb
+    },
+    {
+        name: "Express",
+        img: svgs.express
+    },
+    {
+        name: "Node",
+        img: svgs.nodejs
+    },
+    {
+        name: "Adobe Suite",
+        img: ""
+    },
+];
+
+export default data;

6dddaab599a5571a005735c7aa02a93384e37d30
Andrew Hansen <andrew89hansen@gmail.com>
2021-06-06 18:54:13 -0400

    update to nav

--- frontend/src/Components/Nav/index.js
+++ frontend/src/Components/Nav/index.js
@@ -5,9 +5,21 @@ const Nav = () => {
     console.log(styles);
     return (
         <nav className={styles.nav}>
-            <div className={styles.item}>                
-                <span className={styles.itemText}>Text</span>
-            </div>
+                <a className={styles.link} href={"https://github.com/mbsAndrew"} target={"_blank"}>
+                    <div className={styles.text}>
+                        Github
+                    </div>
+                </a>
+                <a className={styles.link} href={"https://dribbble.com/dowhathappens"} target={"_blank"}>
+                    <div className={styles.text}>
+                        Dribbble
+                    </div>
+                </a>
+                <a className={styles.link} href={"https://drive.google.com/uc?export=download&id=1wXh72I36JWssbie0yLWu3EKyQ876F6d5"} download>
+                    <div className={styles.text}>
+                        Resume
+                    </div>
+                </a>            
         </nav>
     );
 };

becf2d9bd01bc0598c3e158dcd5e97b06998eaee
Andrew Hansen <andrew89hansen@gmail.com>
2021-06-08 15:34:10 -0400

    added email routes.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const music = require("./MusicRoutes");
 const photos = require("./PhotoRoutes");
 const projects = require("./ProjectRoutes");
+const email = require("./EmailRoutes");
 
 router.use("/music", music);
 router.use("/photos", photos);
 router.use("/projects", projects);
+router.use("/email", email);
 
 module.exports = router;

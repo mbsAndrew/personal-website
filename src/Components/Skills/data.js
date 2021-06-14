92f149745149dfad4726878eaf9cc837a47ecd13
Andrew Hansen <andrew89hansen@gmail.com>
2021-06-14 15:46:42 -0400

    modify skills to include years.

--- src/Components/Skills/data.js
+++ src/Components/Skills/data.js
@@ -4,39 +4,49 @@ import svgs from '../../assets/logo/index';
 const data = [
     {
         name: "JavaScript",
-        img: svgs.javascript
+        img: svgs.javascript,
+        start: 2014          
     }, 
     {
         name: "React",
-        img: svgs.react
+        img: svgs.react,
+        start: 2019        
     },    
     {
         name: "CSS",
-        img: svgs.css
+        img: svgs.css,
+        start: 2015        
     },
     {
         name: "Sass",
-        img: svgs.sass
+        img: svgs.sass,
+        start: 2019        
     },
     {
         name: "jQuery",
-        img: svgs.jquery
+        img: svgs.jquery,
+        start: 2014,
+        end: 2019        
     },
     {
         name: "MongoDB",
-        img: svgs.mongodb
+        img: svgs.mongodb,
+        start: 2019        
     },
     {
         name: "Express",
-        img: svgs.express
+        img: svgs.express,
+        start: 2019        
     },
     {
         name: "Node",
-        img: svgs.nodejs
+        img: svgs.nodejs,
+        start: 2019        
     },
     {
         name: "Adobe Suite",
-        img: ""
+        img: svgs.adobe,
+        start: 2019        
     },
 ];
 

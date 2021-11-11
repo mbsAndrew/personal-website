2e9c495597577bb3d201b53eda401930f7915f59
mbsAndrew <andrew89hansen@gmail.com>
2021-11-10 23:23:26 -0500

    link correction.

--- frontend/src/Components/Projects/ProjectList.js
+++ frontend/src/Components/Projects/ProjectList.js
@@ -6,7 +6,7 @@ const ProjectList = ({ projects }) => {
     return (
         <div className={styles.list}>
             {projects.map(project => {
-                return <a href={project.html_url} target={"_blank"}>
+                return <a href={project.html_url} target={"_blank"} rel={"noreferrer"}>
                     <div className={styles.project}>
                     <h4 className={styles.name}>{project.name}</h4>
                     <Languages project={project} />
